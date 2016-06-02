421630ce183f8e0fbe9da859a9527f62514bfa14
Itamar Hassin <ihassin@mac.com>
2016-06-02 08:30:41 -0400

    Added comment on agile

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -13,14 +13,14 @@ class Subject < ActiveRecord::Base
     Subject.find_or_create_by!(title: 'CI',                       x:  850, y:  250, comments: 'Fan-in, fan-out, packaging, promotion')
     Subject.find_or_create_by!(title: 'Stable',                   x: 1300, y:  250, comments: 'Meh.', terminal: 1)
 
-    Subject.find_or_create_by!(title: 'TDD',                      x:  450, y:  350, comments: 'TDD Unit, integration, functional, performance, security')
+    Subject.find_or_create_by!(title: 'TDD',                      x:  470, y:  350, comments: 'TDD Unit, integration, functional, performance, security')
     Subject.find_or_create_by!(title: 'Emergent Arch',            x:  650, y:  350, comments: 'Last possible responsible moment.')
     Subject.find_or_create_by!(title: 'Small batches',            x:  850, y:  350, comments: 'Reduce change sets to a minimum')
     Subject.find_or_create_by!(title: 'AB Testing',               x:  650, y:  550, comments: 'Only if we have a feedback cycle.')
 
     Subject.find_or_create_by!(title: 'MVP',                      x:   10, y:  450, comments: 'Zappos')
     Subject.find_or_create_by!(title: 'BDD',                      x:  250, y:  450, comments: 'Common business language, DDD')
-    Subject.find_or_create_by!(title: 'Regression',               x:  500, y:  450, comments: 'Safety-net')
+    Subject.find_or_create_by!(title: 'Regression',               x:  520, y:  450, comments: 'Safety-net')
     Subject.find_or_create_by!(title: 'CD',                       x: 1000, y:  450, comments: 'end-to-end, economics')
     Subject.find_or_create_by!(title: 'Economical',               x: 1300, y:  450, comments: 'Economical', terminal: 1)
 
@@ -33,6 +33,6 @@ class Subject < ActiveRecord::Base
 
     Subject.find_or_create_by!(title: 'Feature Teams',            x:  100, y:  750, comments: 'Break silos, end-to-end, co-located')
     Subject.find_or_create_by!(title: 'You build it, you run it', x:  450, y:  750, comments: 'Comment about you build it')
-    Subject.find_or_create_by!(title: 'agile',                    x: 1300, y:  750, comments: 'Small a', terminal: 1)
+    Subject.find_or_create_by!(title: 'agile',                    x: 1300, y:  750, comments: 'Small a, innovate!', terminal: 1)
   end
 end
