ed06bf8c29dc5a0839ee730fdeeafe55ecc89553
Itamar Hassin <ihassin@mac.com>
2018-04-21 09:22:28 -0400

    Added path for phantom for jeknins

--- features/support/env.rb
+++ features/support/env.rb
@@ -30,7 +30,8 @@ end
 Capybara.register_driver :poltergeist do |app|
   Capybara::Poltergeist::Driver.new(
       app,
-      window_size: [1280, 1024]
+      window_size: [1280, 1024],
+      phantomjs: '/usr/local/bin/phantomjs'
   )
 end
 Capybara.default_driver    = :poltergeist
