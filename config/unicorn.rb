fb61b03291d1282a8d3f66235f0f5032339c6d59
Itamar Hassin <ihassin@mac.com>
2018-04-26 07:44:02 -0400

    Changed nginx configs

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -3,6 +3,6 @@ pid '/home/pi/fragileagile/current/tmp/pids/unicorn.pid'
 stderr_path '/home/pi/fragileagile/current/log/unicorn.log'
 stdout_path '/home/pi/fragileagile/current/log/unicorn.log'
 
-listen '/tmp/unicorn.fragileagile.sock'
+listen '/tmp/unicorn.fragileagile.sock', :backlog => 64
 worker_processes 2
 timeout 30
